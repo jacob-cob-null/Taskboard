4d9f579f267a36b4cd7df9d427ed5e85d41491e0
PC <silveriolance06@gmail.com>
2026-01-10 21:43:10 +0800

    Refactor: separated server and client components in dashboard, fixed naming convention of dashboard and login page components

--- app/(auth)/login/components/SignInButton.tsx
+++ app/(auth)/login/components/SignInButton.tsx
@@ -1,6 +1,5 @@
-import React from "react";
 import { useState } from "react";
-import { handleSignInWithGoogle } from "@/app/(auth)/login/actions";
+import { handleSignInWithGoogle } from "../actions";
 import { FcGoogle } from "react-icons/fc";
 
 function SignInBtn() {
@@ -19,7 +18,7 @@ function SignInBtn() {
       className="px-4 py-2 bg-white outline-1 outline-slate-300 shadow-2xs text-black rounded flex items-center gap-4 justify-between"
     >
       <FcGoogle className="text-2xl" />
-      <h1>Sign in with Google</h1>
+      <span>Sign in with Google</span>
     </button>
   );
 }
--- app/dashboard/[userId]/layout.tsx
+++ app/dashboard/[userId]/layout.tsx
@@ -1,7 +1,6 @@
-"use client";
-import Top_panel from "./components/top_panel";
-import NewMember from "./components/new_member";
-import { signOut } from "@/app/(auth)/login/actions";
+import TopPanel from "./components/TopPanel";
+import NewMember from "./components/NewMember";
+import SignOutBtn from "./components/SignOutBtn";
 export default function DashboardLayout({
   children,
 }: Readonly<{
@@ -11,13 +10,8 @@ export default function DashboardLayout({
     <div className="h-screen w-full grid grid-rows-[70px_1fr]">
       <div className="p-3">
         <NewMember />
-        <Top_panel />
-        <button
-          onClick={() => signOut()}
-          className="h-fit w-fit p-1 px-2 m-5 bg-red-100 justify-center items-center"
-        >
-          Sign Out
-        </button>
+        <TopPanel />
+        <SignOutBtn />
         {children}
       </div>
     </div>
